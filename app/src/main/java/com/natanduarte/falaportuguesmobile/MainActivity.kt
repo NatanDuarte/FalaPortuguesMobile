512232a6a799420ea7db3545454b99720fff4a2c
NatanDuarte <natanprocopio@gmail.com>
2023-09-19 11:21:06 -0300

    binding functionalities + refactoring

--- app/src/main/java/com/natanduarte/falaportuguesmobile/MainActivity.kt
+++ app/src/main/java/com/natanduarte/falaportuguesmobile/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.natanduarte.falaportuguesmobile
 
+import android.annotation.SuppressLint
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -11,6 +14,7 @@ import com.natanduarte.falaportuguesmobile.rest.RetrofitService
 import com.natanduarte.falaportuguesmobile.util.JsonLoader
 import com.natanduarte.falaportuguesmobile.viewmodel.main.MainViewModel
 import com.natanduarte.falaportuguesmobile.viewmodel.main.MainViewModelFactory
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +23,11 @@ class MainActivity : AppCompatActivity() {
     lateinit var viewModel: MainViewModel
 
     private val retrofitService = RetrofitService.getInstance()
+
+    private var adjective: String = ""
+
+    private var randomNoun: String = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,21 +39,41 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(
             this,
             MainViewModelFactory(MainRepository(retrofitService))
-        ).get(MainViewModel::class.java)
+        )[MainViewModel::class.java]
+
+        binding.mainTerm.setOnClickListener {
+            copyTextToClipboard(binding.mainTerm.text.toString())
+        }
+
+        binding.newTrend.setOnClickListener {
+            randomNoun = getRandomNoun()
+            viewModel.getAdjective()
+        }
+
+        binding.newAdjective.setOnClickListener {
+            viewModel.getAdjective()
+        }
+
+        binding.newNoun.setOnClickListener {
+            randomNoun = getRandomNoun()
+            updateMainTerm()
+        }
     }
 
     override fun onStart() {
         super.onStart()
 
-        viewModel.adjectives.observe(this) { adjectives ->
-            Log.i("DEV", "onStart: $adjectives")
+        randomNoun = getRandomNoun()
 
-            val jsonLoader = JsonLoader(this)
-            val jsonObject = jsonLoader.loadJsonFileToArray("job-nouns.json")
+        viewModel.adjectives.observe(this) { adjectives ->
+            adjective = adjectives[0]
+            updateMainTerm()
         }
 
         viewModel.errorMessage.observe(this) { message ->
-            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+            Toast
+                .makeText(this, message, Toast.LENGTH_SHORT)
+                .show()
         }
     }
 
@@ -52,5 +81,30 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
 
         viewModel.getAdjective()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateMainTerm() {
+        binding.mainTerm.text = "$adjective $randomNoun"
+    }
+
+    private fun copyTextToClipboard(textToCopy: String) {
+        val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+
+        val clipData = ClipData.newPlainText("Texto copiado", textToCopy)
+        clipboardManager.setPrimaryClip(clipData)
+
+        Toast
+            .makeText(this, "Texto copiado para a área de transferência", Toast.LENGTH_SHORT)
+            .show()
+    }
+
+    private fun getRandomNoun(): String {
+        val jsonLoader = JsonLoader(this)
+        val jobNouns = jsonLoader.loadJsonFileToArray("job-nouns.json")
+
+        val random = Random()
+        val randomIndex = random.nextInt(jobNouns!!.length())
+        return jobNouns[randomIndex].toString()
     }
 }
